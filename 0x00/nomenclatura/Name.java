3edd660c32dafd1400c7fea6e426280d01afa50b
Jesse Machado <jesse.machado@bradesco.com.br>
2025-12-15 12:25:23 -0300

    Add initial implementation of Name class with user and account management methods

---
+++ 0x00/nomenclatura/Name.java
@@ -0,0 +1,64 @@
+import java.net.URL;
+import java.util.Date;
+import java.util.List;
+
+public class Name {
+    
+    // Variáveis que falam por si só
+    public Date modifiedDate;
+    
+    public List<Testcase> findAllTestcasesByUser(User user) {
+        // Implementation
+        return null;
+    }
+    
+    // Declarações corretas
+    public List<Account> accountList;
+    
+    // Sem Prefixos
+    public void deleteUser(User user) {
+        // Implementation
+    }
+    
+    // Evite mapa mental
+    public URL url;
+    
+    // Classe possui nomes próprios, funções devem ter verbos
+    public class TableRepresent {
+        // Implementation
+    }
+    
+    public void saveCar() {
+        // Implementation
+    }
+    
+    // Uma palavra por conceito
+    public void saveUser() {
+        // Implementation
+    }
+    
+    public void saveAccount() {
+        // Implementation
+    }
+    
+    public void saveContract() {
+        // Implementation
+    }
+    
+    // Use constants, enums instead of magic numbers, abbreviations
+    public static final int DAYS_IN_YEAR = 360;
+    public static final int HOURS_IN_WEEK = 168;
+    
+    // Inner classes
+    public class User {
+        // Implementation
+    }
+    
+    public class Account {
+        // Implementation
+    }
+    
+    public class Testcase {
+        // Implementation
+    }
+}
