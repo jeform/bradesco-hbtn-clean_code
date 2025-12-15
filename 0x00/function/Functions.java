f58c963545564bd64109797c69aee1910aa54479
Jesse Machado <jesse.machado@bradesco.com.br>
2025-12-15 12:28:16 -0300

    Implement Functions class with methods for income, expense, and user management

---
+++ 0x00/function/Functions.java
@@ -0,0 +1,72 @@
+import org.springframework.web.bind.annotation.PostMapping;
+import java.util.Date;
+
+public class Functions {
+    
+    // Faça somente 1 coisa
+    public void saveIncome(Income income) {
+        inRepo.save(income);
+    }
+    
+    public void saveExpense(Expense expense) {
+        outRepo.save(expense);
+    }
+
+    // Somente 1 nível de abstração
+    @PostMapping("/user")
+    public User saveUser(User user) {
+        return userService.save(user);
+    }
+
+    // Arguments
+    public void saveUser(User user) {
+        // Implementation
+    }
+
+    // No side effects
+    public User saveUser(User user) {
+        return userRepository.save(user);
+    }
+    
+    public void setRole(User user) {
+        user.setRole(user);
+    }
+
+    // Evite duplicados, extraindo para novos métodos
+    public Environment getEnvironment() {
+        return environmentService.getEnvironment(getVersion());
+    }
+    
+    public Release getRelease() {
+        return releaseService.getRelease(getVersion());
+    }
+    
+    private String getVersion() {
+        return versionService.getVersion();
+    }
+
+    // Inner classes and dependencies
+    public class Income {
+        // Implementation
+    }
+    
+    public class Expense {
+        // Implementation
+    }
+    
+    public class User {
+        public boolean isAdmin;
+        
+        public void setRole(User user) {
+            // Implementation
+        }
+    }
+    
+    public class Environment {
+        // Implementation
+    }
+    
+    public class Release {
+        // Implementation
+    }
+}
